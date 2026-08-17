2f31be3b801f8423ab1a3d131b4a3b42e4e5a2be
gabrielsousa <imgabrielsousa2@gmail.com>
2026-08-17 01:55:41 -0300

    atualizando posicionamento dos elementos no header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
+const headerIndex = document.getElementById('headerIndex')
 const btnMenu = document.getElementById('btnMenu')
 const itensMenu = document.getElementById('itensMenu')
 
 btnMenu.addEventListener('click', () => {
-
-    itensMenu.classList.remove('menu-invisible')
-    itensMenu.classList.add('menu-visible')
-    btnMenu.classList.remove('menu-invisible')
-    btnMenu.classList.add('menu-visible')
+    itensMenu.classList.toggle('nav-visible')/* 
+    headerIndex.classList.toggle('header-grid') */
 });
     
