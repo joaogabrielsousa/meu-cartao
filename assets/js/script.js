4675568e1a87d24129602bdac1c57720fb59433c
gabrielsousa <imgabrielsousa2@gmail.com>
2026-08-18 03:07:11 -0300

    atualização de estilos do Menu concluida

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
-const headerIndex = document.getElementById('headerIndex')
 const btnMenu = document.getElementById('btnMenu')
 const itensMenu = document.getElementById('itensMenu')
 
 btnMenu.addEventListener('click', () => {
-    itensMenu.classList.toggle('nav-visible')/* 
-    headerIndex.classList.toggle('header-grid') */
+    itensMenu.classList.toggle('menu-off')
 });
     
